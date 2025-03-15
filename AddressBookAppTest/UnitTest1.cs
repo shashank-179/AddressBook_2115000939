d77fd1159883612e8f10932a3a60316015c56379
shashank-179 <shashank17903@gmail.com>
2025-03-16 04:38:41 +0530

    [Shashank Mishra]: ADD: Adding Section 5-UC2

---
+++ AddressBookAppTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AddressBookAppTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
